4f0d97c7a80b99c5ec9aedcabe0262f55bf386fb
user uaers <thatkrsn21@gmail.com>
2023-09-16 13:12:53 +0300

    Добавли задание 3

---
+++ task3/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран
+
+int[] numbers = new int[8] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+
+Console.WriteLine();
+for (int i = 0; i < numbers.Length; i++)
+{
+ Console.Write(numbers[i] + " ");
+}
+
+Console.WriteLine();
